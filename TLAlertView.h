c44c7f7f2a42de42ae186d8e92df592a43afeb1a
Adrian Mayoral <mayoralito@gmail.com>
2013-10-21 15:57:01 -0500

    Added methods initWithView... to handle UIView inside our custom TLAlertView and demo

--- TLAlertView.h
+++ TLAlertView.h
@@ -15,13 +15,17 @@ typedef void(^TLAlertViewHandler)(TLAlertView *alertView);
 
 @interface TLAlertView : UIView
 
-@property (nonatomic, readonly) NSString *title;
-@property (nonatomic, readonly) NSString *message;
-@property (nonatomic, readonly) NSString *buttonTitle;
+@property (nonatomic, readonly) NSString    *title;
+@property (nonatomic, readonly) NSString    *message;
+@property (nonatomic, readonly) NSString    *buttonTitle;
+@property (nonatomic, readonly) UIView      *customAlertView;
 
 -(instancetype)initWithTitle:(NSString *)title message:(NSString *)message buttonTitle:(NSString *)buttonTitle;
 -(instancetype)initWithTitle:(NSString *)title message:(NSString *)message buttonTitle:(NSString *)buttonTitle outsideClose: (BOOL)tap2close;
 -(instancetype)initWithTitle:(NSString *)title message:(NSString *)message buttonTitle:(NSString *)buttonTitle handler:(TLAlertViewHandler)handler;
+-(instancetype)initWithView:(UIView *)view;
+-(instancetype)initWithView:(UIView *)view outsideClose: (BOOL)tap2close;
+-(instancetype)initWithView:(UIView *)view handler:(TLAlertViewHandler)handler;
 
 -(void)show;
 -(void)dismiss;
